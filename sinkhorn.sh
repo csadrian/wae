fa8e84e478e83a17a57f6bf8da9fc6dcc604b42a
csadrian <cs.adrian@gmail.com>
2019-08-23 00:54:10 +0200

    Corrected experiment

--- sinkhorn.sh
+++ sinkhorn.sh
@@ -9,7 +9,7 @@ for ot_lambda in 10000.0
 do
 for zdim in 10
 do
-name="nobn_logdomain_train_size=${train_size}_rec_lambda=${rec_lambda}_zdim=${zdim}__sinkhorn_iters=${sinkhorn_iters}_sinkhorn_epsilon=${sinkhorn_epsilon}_ot_lambda=${ot_lambda}"
+name="yesbn_logdomain_train_size=${train_size}_rec_lambda=${rec_lambda}_zdim=${zdim}__sinkhorn_iters=${sinkhorn_iters}_sinkhorn_epsilon=${sinkhorn_epsilon}_ot_lambda=${ot_lambda}"
 python run.py --rec_lambda=${rec_lambda} --train_size ${train_size} --wae_lambda=0.0 --epoch_num=1000 --ot_lambda=${ot_lambda} --enc_noise=deterministic --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} --zdim=${zdim} --e_pretrain=False --work_dir=sinkhorn_${name}  > sinkhorn_${name}.cout 2> sinkhorn_${name}.cerr
 done
 done
