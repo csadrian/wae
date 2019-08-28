52939bccb9e04e81b41c83684c52c980c90b97d2
csadrian <cs.adrian@gmail.com>
2019-08-28 19:30:09 +0200

    Added temp exp

---
+++ sinkhorn_a.sh
@@ -0,0 +1,24 @@
+
+mkdir -p out
+
+dt=$(date '+%d%m%Y%H%M%S');
+echo $dt
+
+rec_lambda=1.0
+train_size=100000
+exp=celebA
+
+for sinkhorn_iters in 20
+do
+for sinkhorn_epsilon in 1.0
+do
+for ot_lambda in 100.0
+do
+for zdim in 64
+do
+name="megabatch_exp=${exp}_logdomain_train_size=${train_size}_rec_lambda=${rec_lambda}_zdim=${zdim}__sinkhorn_iters=${sinkhorn_iters}_sinkhorn_epsilon=${sinkhorn_epsilon}_ot_lambda=${ot_lambda}_${dt}"
+python run.py --nat_size=1000 --tag="megabatch,pretrain" --exp=${exp} --rec_lambda=${rec_lambda} --train_size ${train_size} --wae_lambda=0.0 --epoch_num=100 --ot_lambda=${ot_lambda} --enc_noise=deterministic --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} --zdim=${zdim} --e_pretrain=False --work_dir=out/sinkhorn_${name}  > out/sinkhorn_${name}.cout 2> out/sinkhorn_${name}.cerr
+done
+done
+done
+done
