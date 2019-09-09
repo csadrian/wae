ad28dd05ffcd3f1948bc545fe6b3653229c430a2
Daniel Varga <daniel.varga@prezi.com>
2019-09-09 12:40:14 +0200

    imq mmd

--- mmd_test.py
+++ mmd_test.py
@@ -38,6 +38,30 @@ def mmd_penalty(sample_qz, sample_pz, pz_scale, kernel='RBF'):
         res2 = tf.exp( - distances / 2. / sigma2_k)
         res2 = tf.reduce_sum(res2) * 2. / (nf * nf)
         stat = res1 - res2
+    elif kernel == 'IMQ':
+        # k(x, y) = C / (C + ||x - y||^2)
+        # C = tf.nn.top_k(tf.reshape(distances, [-1]), half_size).values[half_size - 1]
+        # C += tf.nn.top_k(tf.reshape(distances_qz, [-1]), half_size).values[half_size - 1]
+        pz_kind = 'normal'
+        if pz_kind == 'normal':
+            Cbase = 2. * d * sigma2_p
+        elif pz_kind == 'sphere':
+            Cbase = 2.
+        elif pz_kind == 'uniform':
+            # E ||x - y||^2 = E[sum (xi - yi)^2]
+            #               = zdim E[(xi - yi)^2]
+            #               = const * zdim
+            Cbase = d
+        stat = 0.
+        for scale in [.1, .2, .5, 1., 2., 5., 10.]:
+            C = Cbase * scale
+            res1 = C / (C + distances_qz)
+            res1 += C / (C + distances_pz)
+            res1 = tf.multiply(res1, 1. - tf.eye(n))
+            res1 = tf.reduce_sum(res1) / (nf * nf - nf)
+            res2 = C / (C + distances)
+            res2 = tf.reduce_sum(res2) * 2. / (nf * nf)
+            stat += res1 - res2
     else:
         assert False
 
@@ -56,7 +80,7 @@ def main():
         scale = tf.Variable(1.0, dtype=tf.float32)
         sample_qz = scale * tf.random.normal((n, d), dtype=tf.float32)
         sample_pz = tf.random.normal((n, d), dtype=tf.float32)
-        mmd = mmd_penalty(sample_qz, sample_pz, pz_scale=1.0, kernel='RBF')
+        mmd = mmd_penalty(sample_qz, sample_pz, pz_scale=1.0, kernel='IMQ')
         e(tf.global_variables_initializer())
         for scale_np in np.linspace(-2, +2, 21):
             print(scale_np, sess.run(mmd, feed_dict={scale: scale_np}))
