15affddd6c166315ca9e2d1d464a2da8eabedde9
csadrian <cs.adrian@gmail.com>
2019-09-12 20:15:59 +0200

    Added exp

---
+++ final/sinkhorn_syn_megab_a.sh
@@ -0,0 +1,27 @@
+mkdir -p out
+
+dt=$(date '+%d%m%Y%H%M%S');
+echo $dt
+
+rec_lambda=1.0
+train_size=10000
+exp=syn_2_constant_uniform
+nat_resampling=None
+wae_lambda=0.0
+epoch_num=100
+pz=uniform
+nat_size=1000
+for sinkhorn_iters in 10
+do
+for sinkhorn_epsilon in 0.1 1.0 10.0 0.01 0.001
+do
+for ot_lambda in 1.0 10.0 100.0 0.1
+do
+for zdim in 2
+do
+name="syn_2c_res=${nat_resampling}_exp=${exp}_train_size=${train_size}_rec_lambda=${rec_lambda}_zdim=${zdim}_sinkhorn_iters=${sinkhorn_iters}_sinkhorn_epsilon=${sinkhorn_epsilon}_ot_lambda=${ot_lambda}_${dt}"
+python run.py --pz=${pz} --train_size=${train_size} --sinkhorn_sparse=False --enc_noise=deterministic --name="${name}" --nat_size=${nat_size} --nat_resampling=${nat_resampling} --tag="syn,syn_2c" --exp=${exp} --rec_lambda=${rec_lambda} --wae_lambda=${wae_lambda} --epoch_num=${epoch_num} --ot_lambda=${ot_lambda} --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} --zdim=${zdim} --e_pretrain=False --work_dir=out/sinkhorn_${name}  > out/sinkhorn_${name}.cout 2> out/sinkhorn_${name}.cerr
+done
+done
+done
+done
