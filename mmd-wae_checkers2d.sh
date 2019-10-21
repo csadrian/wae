f9b5889df468c43dc1ee4c7a8c8d50278d495d61
Daniel Varga <daniel.varga@prezi.com>
2019-10-21 02:00:37 +0200

    job scripts. wae-mmd varying batchsize and lr, global-sinkhorn varying folc.

---
+++ mmd-wae_checkers2d.sh
@@ -0,0 +1,33 @@
+
+mkdir -p out
+
+dt=$(date '+%d%m%Y%H%M%S');
+echo $dt
+
+rec_lambda=1.0
+train_size=10000
+exp=checkers
+sinkhorn_iters=10
+epoch_num=20
+
+
+for bs in 100 200 400 500 1000
+do
+for lr in 0.0001 0.0003
+do
+wae_lambda=100
+zdim=2
+sinkhorn_iters=10
+sinkhorn_epsilon=0.01
+name="exp=${exp}_wae_bstuning_train_size=${train_size}_zdim=${zdim}_wae_lambda=${wae_lambda}_bs=${bs}_lr=${lr}_${dt}"
+python run.py \
+    --sinkhorn_sparse=False --sinkhorn_sparsifier=None --nat_sparse_indices_num=1000 \
+    --pz=uniform --zdim=${zdim} \
+    --enc_noise=deterministic --name="${name}" --nat_size=${train_size} \
+    --nat_resampling=${nat_resampling} --tag="${exp},checkers_2_grid,wae,bstuning2" --exp=${exp} \
+    --rec_lambda=${rec_lambda} --train_size=${train_size} --wae_lambda=${wae_lambda} --epoch_num=${epoch_num} \
+    --batch_size=${bs} --lr=${lr} --frequency_of_latent_change=1 \
+    --ot_lambda=0.0 --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} \
+    --e_pretrain=False --work_dir=out/wae_${name}  > out/wae_${name}.cout 2> out/wae_${name}.cerr
+done
+done
---
+++ sinkhorn_checkers2d_n1e5_folc.sh
@@ -0,0 +1,38 @@
+
+mkdir -p out
+
+dt=$(date '+%d%m%Y%H%M%S');
+echo $dt
+
+rec_lambda=1.0
+train_size=10000
+exp=checkers
+sinkhorn_iters=10
+epoch_num=10
+
+
+for nat_resampling in None
+do
+for sinkhorn_epsilon in 0.01
+do
+for ot_lambda in 0.1
+do
+for zdim in 2
+do
+for frequency_of_latent_change in 0 1 5 10 20 50
+do
+name="exp=${exp}_global_res=${nat_resampling}_train_size=${train_size}_folc=${frequency_of_latent_change}_rec_lambda=${rec_lambda}_zdim=${zdim}_sinkhorn_epsilon=${sinkhorn_epsilon}_ot_lambda=${ot_lambda}_${dt}"
+python run.py \
+    --sinkhorn_sparse=False --sinkhorn_sparsifier=None --nat_sparse_indices_num=1000 \
+    --pz=uniform --zdim=${zdim} \
+    --enc_noise=deterministic --name="${name}" --nat_size=${train_size} \
+    --nat_resampling=${nat_resampling} --tag="${exp},checkers_2_grid,global_dense" --exp=${exp} \
+    --rec_lambda=${rec_lambda} --train_size=${train_size} --wae_lambda=0.0 --epoch_num=${epoch_num} \
+    --ot_lambda=${ot_lambda} --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} \
+    --frequency_of_latent_change=${frequency_of_latent_change} \
+    --e_pretrain=False --work_dir=out/sinkhorn_${name}  > out/sinkhorn_${name}.cout 2> out/sinkhorn_${name}.cerr
+done
+done
+done
+done
+done
