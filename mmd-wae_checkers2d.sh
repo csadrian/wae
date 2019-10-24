90f76ed64eaaba99208e7bfb5069fcc39cfd3dbe
Daniel Varga <daniel.varga@prezi.com>
2019-10-24 18:03:42 +0200

    bstuning4 wae-mmd experiment.

--- mmd-wae_checkers2d.sh
+++ mmd-wae_checkers2d.sh
@@ -1,37 +1,40 @@
-
 mkdir -p out
 
 dt=$(date '+%d%m%Y%H%M%S');
 echo $dt
 
-rec_lambda=1.0
-train_size=10000
-exp=checkers
-sinkhorn_iters=10
-epoch_num=20
 
-mmd_or_sinkhorn = 'mmd'
-mmd_linear = False
-
-
-for bs in 100 200 400 500 1000
+for bs in 1000 500 400 200 100 50
 do
 for lr in 0.0001 0.0003
 do
-wae_lambda=0
-ot_lambda=100
+
+exp=checkers
 zdim=2
+wae_lambda=100.0
+train_size=10000
+recalculate_size=10000
+epoch_num=200
+mmd_or_sinkhorn='mmd'
+
+pz=uniform
+rec_lambda=1.0
 sinkhorn_iters=10
 sinkhorn_epsilon=0.01
-name="exp=${exp}_wae_bstuning_train_size=${train_size}_zdim=${zdim}_wae_lambda=${wae_lambda}_bs=${bs}_lr=${lr}_${dt}"
-python run.py \
-    --sinkhorn_sparse=False --sinkhorn_sparsifier=None --nat_sparse_indices_num=1000 \
-    --pz=uniform --zdim=${zdim} \
-    --enc_noise=deterministic --name="${name}" --nat_size=${train_size} \
-    --nat_resampling=${nat_resampling} --tag="${exp},checkers_2_grid,wae,bstuning2" --exp=${exp} \
-    --rec_lambda=${rec_lambda} --train_size=${train_size} --wae_lambda=${wae_lambda} --epoch_num=${epoch_num} \
-    --batch_size=${bs} --lr=${lr} --frequency_of_latent_change=1 \
-    --ot_lambda=0.0 --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} \
-    --e_pretrain=False --work_dir=out/wae_${name}  > out/wae_${name}.cout 2> out/wae_${name}.cerr
+ot_lambda=0
+nat_resampling=None
+# loss_mmd (a monitored but not optimized value) is calculated with this many prior samples:
+nat_size=${train_size}
+mmd_linear=False
+
+
+name="wae-mmd_res=${nat_resampling}_exp=${exp}_batch_size=${bs}_lr=${lr}_train_size=${train_size}_rec_lambda=${rec_lambda}_sinkhorn_epsilon=${sinkhorn_epsilon}_ot_lambda=${ot_lambda}_mmd_or_sinkhorn=${mmd_or_sinkhorn}_${dt}"
+CUDA_VISIBLE_DEVICES=3 python run.py --pz=${pz} --train_size=${train_size} --mmd_or_sinkhorn=${mmd_or_sinkhorn} \
+    --mmd_linear=${mmd_linear} --sinkhorn_sparse=False --enc_noise=deterministic --name="${name}" \
+    --nat_size=${nat_size} --nat_resampling=${nat_resampling} --tag="syn,syn_2c,wae,bstuning4" \
+    --exp=${exp} --rec_lambda=${rec_lambda} --wae_lambda=${wae_lambda} --epoch_num=${epoch_num} \
+    --ot_lambda=${ot_lambda} --sinkhorn_epsilon=${sinkhorn_epsilon} --sinkhorn_iters=${sinkhorn_iters} \
+    --zdim=${zdim} --e_pretrain=False --batch_size=${bs} --lr=${lr} --work_dir=out/sinkhorn_${name} \
+        > out/${name}.cout 2> out/${name}.cerr
 done
 done
