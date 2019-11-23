537766075d5b909e8ff8f4b9a486b106195aadb9
melindafkiss <melindafkiss@gmail.com>
2019-11-23 08:12:38 +0100

    Scatter plot

--- stats.py
+++ stats.py
@@ -1,7 +1,11 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from neptune import Session
 import numpy as np
+import pandas as pd
+
 
 session = Session()
 project = session.get_projects('csadrian')['csadrian/global-sinkhorn']
@@ -26,7 +30,7 @@ for z_test in {'mmd', 'sinkhorn'}:
             crits['{:}_{:}_{:}'.format(z_test, z_test_scope, wae_lambda)]={'z_test': z_test, 'z_test_scope' : z_test_scope, 'lambda': wae_lambda}
 
 #channels = ['covered_area', 'mmd', 'sinkhorn_ot', 'loss_rec']
-channels = ['covered_area']
+channels = ['covered_area', 'loss_rec']
 
 all_exps = project.get_experiments()
 
@@ -53,29 +57,57 @@ for key, crit in crits.items():
       if channel in vals:
         res[channel].append(float(vals[channel]['y']))
 
-
+  means=[] 
   for channel in channels:
     v = np.array(res[channel])
    # if v.shape[0] > 5:
    #   print("{}: Warning: more than 5 experiments: {} using only 5 (no order assumed)".format(key, v.shape[0]))
    # v = v[:5]
     mean = np.mean(v)
-    results.append([crit, mean])
+    means.append(mean)
+   # results.append([crit, mean])
     std = np.std(v)
     cnt = v.shape[0]
    # print("{:} {} mean: {:.2f}, std: {:.2f}, cnt: {}".format(key, channel, mean, std, cnt))
+  results.append([crit, means[0], means[1]])
+#print(results)
 
-print(results)
+scopedict = {}
+scopedict[('mmd', 'global')] = 0
+scopedict[('mmd', 'local')] = 1
+scopedict[('sinkhorn', 'global')] = 2
+scopedict[('sinkhorn', 'local')] = 3
 
-results
+tables = []
 improved_results = []
 for z_test in {'mmd', 'sinkhorn'}:
     for z_test_scope in {'global', 'local'}:
-        results_sliced=[result[1] for result in results if result[0]['z_test']==z_test and result[0]['z_test_scope']==z_test_scope]
-        results_sliced=np.stack(results_sliced)
-        optimal_value=np.max(results_sliced)
-        optimal_place=np.argmax(results_sliced)
-        optimal_lambda=results[optimal_place][0]['lambda']
-        improved_results.append([z_test, z_test_scope, optimal_value, optimal_lambda])
+      results_sliced=[[result[1], result[2], scopedict[(z_test, z_test_scope)]] for result in results if result[0]['z_test']==z_test and result[0]['z_test_scope']==z_test_scope]
+      results_sliced=np.stack(results_sliced)
+      optimal_value_area=np.max(results_sliced[0])
+      optimal_value_rec=np.max(results_sliced[1])
+      optimal_place_area=np.argmax(results_sliced[0])
+      optimal_place_rec=np.argmax(results_sliced[1])
+      optimal_lambda_area=results[optimal_place_area][0]['lambda']
+      optimal_lambda_rec=results[optimal_place_rec][0]['lambda']        
+      improved_results.append([z_test, z_test_scope, optimal_value_area, optimal_value_rec, optimal_lambda_area, optimal_lambda_rec])
+      tables.append(results_sliced)
 
-print(improved_results)
+tables = np.stack(tables)
+tables = [item for sublist in tables for item in sublist]
+#print(tables)
+
+#print(improved_results)
+
+'''
+best_covered = pd.DataFrame(improved_results, columns = ['z_test', 'z_test_scope', 'covered_area', 'lambda'])
+print(best_covered.to_latex())
+#print(best_covered)
+'''
+
+points = pd.DataFrame(tables, columns = ['area', 'rec', 'param'])
+points.plot.scatter(x = 'rec', y = 'area', c = 'param', colormap = 'magma')
+plt.savefig("Scatter.png")
+
+
+
